f799b2876c2c0ddc074e4188cc8ebf1b39d84e72
MoinulIslam7 <moinul180180@gmail.com>
2022-11-07 21:07:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0tydy0p.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    res.status(401).send({message: 'Unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      res.status(401).send({message: 'Unauthorized access'})
+    }
+    req.decoded= decoded;
+    next();
+  })
+
+}
+
+async function run(){
+  try{
+    const serviceCollection = client.db('geniusCar').collection('services');
+    const orderCollection = client.db('geniusCar').collection('orders');
+    
+
+
+    app.post('/jwt', async (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token});
+
+    });
+    app.get('/services', async (req, res) =>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get('/services/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    })
+
+
+
+    // orders api
+    app.post('/orders', async (req, res) =>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+    
+    app.get('/orders', verifyJWT, async (req, res) =>{
+      
+      let query = {};
+
+      if(req.query.email){
+        query = {
+          email: req.query.email
+        }
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    })
+    
+    app.patch('/orders/:id', async (req, res) =>{
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set:{
+          status: status
+        }
+      }
+      const result = await orderCollection.updateOne(query,  updatedDoc);
+      res.send(result);
+    })
+
+
+
+
+    app.delete('/orders/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    })
+  }
+  finally{
+
+  }
+}
+
+run().catch(err => console.error(err))
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Genius car server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Genius car server running on ${port}`);
+})
